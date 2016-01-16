a42c0b72f3fb22f63f92624f962d29ad617d9426
Andrew Ding <afding@gmail.com>
2016-01-16 14:22:48 -0600

    In-between Homework #3 and #4
    Trying to create a new project called RestaurantManager.Extensions
    instead of Extensions.

---
+++ RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? Colors.Red : Colors.Transparent;
+            }
+            else
+            {
+                // We assume it's false if invalid or null.
+                return Colors.Transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            this.AssociatedObject = associatedObject;
+            if (this.AssociatedObject is Page)
+            {
+                (this.AssociatedObject as Page).RightTapped += RightClickBehavior_RightTapped;
+            }
+        }
+
+        private void RightClickBehavior_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            //new MessageDialog("Thank you for trying out this demo!", "Thank you!").ShowAsync();
+            new MessageDialog(Message, Title).ShowAsync();
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Page).RightTapped -= RightClickBehavior_RightTapped;
+        }
+    }
+}
