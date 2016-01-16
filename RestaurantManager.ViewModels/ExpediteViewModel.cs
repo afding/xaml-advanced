d39972ec040e68d437c146e40125292101b86d8a
Andrew Ding <afding@gmail.com>
2016-01-16 15:38:04 -0600

    Homework #4 In Progress
    Created the ViewModels project and moved the relevant files over, and
    used ObservableCollection in place of List.
    Still need to add commands.

---
+++ RestaurantManager.ViewModels/ExpediteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RestaurantManager.Models;
+using System.Collections.ObjectModel;
+
+namespace RestaurantManager.ViewModels
+{
+    public class ExpediteViewModel : ViewModel
+    {
+        protected override void OnDataLoaded()
+        {
+            NotifyPropertyChanged("OrderItems");
+        }
+
+        public ObservableCollection<Order> OrderItems
+        {
+            get { return base.Repository.Orders; }
+        }
+    }
+}
---
+++ RestaurantManager.ViewModels/OrderViewModel.cs
@@ -0,0 +1,34 @@
+﻿using RestaurantManager.Models;
+using System.Collections.ObjectModel;
+
+namespace RestaurantManager.ViewModels
+{
+    public class OrderViewModel : ViewModel
+    {
+        private ObservableCollection<MenuItem> _menuItems;
+        private ObservableCollection<MenuItem> _currentlySelectedMenuItems;
+
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new ObservableCollection<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+
+        public ObservableCollection<MenuItem> MenuItems
+        {
+            get { return this._menuItems; }
+            set { _menuItems = value; }
+        }
+
+        public ObservableCollection<MenuItem> CurrentlySelectedMenuItems
+        {
+            get { return this._currentlySelectedMenuItems; }
+            set { _currentlySelectedMenuItems = value; }
+        }
+    }
+}
