12c407a507d1ebf1f46a50d1fabe4d9dcb24dee9
Andrew Ding <afding@gmail.com>
2016-01-15 19:52:52 -0600

    Homework #3 In Progress
    Added Extensions project and BoolToColorConverter, but haven't used it
    anywhere yet.

---
+++ Extensions/BoolToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace Extensions
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? Colors.Red : Colors.Transparent;
+            }
+            else
+            {
+                // We assume it's false if invalid or null.
+                return Colors.Transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
