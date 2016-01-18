e14ddc1ea8fc281dd9915b19fd3f586301c7500a
Andrew Ding <afding@gmail.com>
2016-01-17 22:42:57 -0600

    Homework #4 In Progress
    Implemented the "Add to Order" and "Submit Order" commands. May
    implement "Clear All Items" command in the future.

--- RestaurantManager.ViewModels/OrderViewModel.cs
+++ RestaurantManager.ViewModels/OrderViewModel.cs
@@ -1,25 +1,37 @@
 ﻿using RestaurantManager.Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Windows.UI.Popups;
 
 namespace RestaurantManager.ViewModels
 {
     public class OrderViewModel : ViewModel
     {
-        protected override void OnDataLoaded()
+        public OrderViewModel()
         {
-            this.MenuItems = new ObservableCollection<MenuItem>(base.Repository.StandardMenuItems);
-
-            this.CurrentlySelectedMenuItems = new ObservableCollection<MenuItem>
-            {
-                this.MenuItems[3],
-                this.MenuItems[5]
-            };
+            AddToOrderCommand = new DelegateCommand(AddToOrderExecute);
+            SubmitOrderCommand = new DelegateCommand(SubmitOrderExecute);
         }
 
+        protected override void OnDataLoaded()
+        {
+            // For some reason, the model thinks base.Repository.StandardMenuItems (or somewhere along the line) is null.
+            // Shrugs.
+            this.MenuItems = new ObservableCollection<MenuItem>(base.Repository.StandardMenuItems);
+            this.CurrentlySelectedMenuItems = new ObservableCollection<MenuItem>();
+        }
+        
         private ObservableCollection<MenuItem> _menuItems;
 
         private ObservableCollection<MenuItem> _currentlySelectedMenuItems;
+
+        private MenuItem _listViewSelectedMenuItem;
+
+        private string _specialRequests;
+
+        public DelegateCommand AddToOrderCommand { get; private set; }
+
+        public DelegateCommand SubmitOrderCommand { get; private set; }
 
         public ObservableCollection<MenuItem> MenuItems
         {
@@ -41,6 +53,72 @@ namespace RestaurantManager.ViewModels
                 this._currentlySelectedMenuItems = value;
                 NotifyPropertyChanged();
             }
+        }
+
+        public MenuItem ListViewSelectedMenuItem
+        {
+            get { return this._listViewSelectedMenuItem; }
+            set
+            {
+                if (this._listViewSelectedMenuItem != value)
+                {
+                    this._listViewSelectedMenuItem = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string SpecialRequests
+        {
+            get { return _specialRequests; }
+            set
+            {
+                if (_specialRequests != value)
+                {
+                    _specialRequests = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void AddToOrderExecute()
+        {
+            if (this.ListViewSelectedMenuItem != null)
+            {
+                this.CurrentlySelectedMenuItems.Add(this.ListViewSelectedMenuItem);
+                // Not sure if this line is necessary, but we're not really "set"ting CurrentlySelectedMenuItems,
+                // so to be safe, I raise the PropertyChanged event.
+                NotifyPropertyChanged("CurrentlySelectedMenuItems");
+            }
+            else
+            {
+                new MessageDialog("Oops. Please select a menu item first.").ShowAsync();
+            }
+        }
+
+        private void SubmitOrderExecute()
+        {
+            // We ignore "orders" with 0 items.
+            if (this.CurrentlySelectedMenuItems != null & this.CurrentlySelectedMenuItems.Count != 0)
+            {
+                Order newOrder = new Order { Items = new List<MenuItem>(this.CurrentlySelectedMenuItems), SpecialRequests = this.SpecialRequests };
+                base.Repository.Orders.Add(newOrder);
+                ClearCurrentOrderItems();
+                // I doubt I need to call PropertyChanged for the Orders in the other ExpediteViewModel.
+            }
+            else
+            {
+                new MessageDialog("Oops. Please add some items to the order first.").ShowAsync();
+            }
+        }
+
+        // Resets the Special Requests and Order Items controls
+        private void ClearCurrentOrderItems()
+        {
+            this.CurrentlySelectedMenuItems.Clear();
+            this.SpecialRequests = string.Empty;
+            NotifyPropertyChanged("CurrentlySelectedMenuItems");
+            NotifyPropertyChanged("SpecialRequests");
         }
     }
 }
